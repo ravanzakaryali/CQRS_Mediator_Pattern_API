ac66752721bcb3f6dfe93c066e100c78c7d7090e
revanzli <revanzekeryeli2002@gmail.com>
2022-05-11 20:20:42 +0400

    feat: added create blog command handler

---
+++ CQRS_Mediator_Pattern_API/Application/Handlers/CommandHandlers/CreateBlogCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Application.Commands.Request;
+using Application.Commands.Response;
+using Domain.Entities;
+using Persistence.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Handlers.CommandHandlers
+{
+    public class CreateBlogCommandHandler
+    {
+        private readonly AppDbContext _context;
+
+        public CreateBlogCommandHandler(AppDbContext context)
+        {
+            _context = context;
+        }
+        public CreateBlogCommandResponse CreateBlog(CreateBlogCommandRequest createBlogRequest)
+        {
+            _context.Blogs.Add(new Blog
+            {
+                Content = createBlogRequest.Content,
+                Title = createBlogRequest.Title,
+                UserId = createBlogRequest.UserId,
+            });
+            return new CreateBlogCommandResponse
+            {
+                UserId = createBlogRequest.UserId,
+                IsSuccess = true,
+            };
+        }
+    }
+}
