c1ebbe5f3b9c887de3d93243e3fd705505b9d538
revanzli <revanzekeryeli2002@gmail.com>
2022-05-11 20:37:59 +0400

    feat: added delete blog command handler

---
+++ CQRS_Mediator_Pattern_API/Application/Handlers/CommandHandlers/DeleteBlogCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using Application.Commands.Request;
+using Application.Commands.Response;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Handlers.CommandHandlers
+{
+    public class DeleteBlogCommandHandler
+    {
+        private readonly AppDbContext _context;
+
+        public DeleteBlogCommandHandler(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<DeleteBlogCommandResponse> DeleteBlog(DeleteBlogCommandRequest deleteBlog)
+        {
+            _context.Blogs.Remove(await _context.Blogs.FirstOrDefaultAsync(p => p.Id == deleteBlog.Id));
+            return new DeleteBlogCommandResponse
+            {
+                Id = deleteBlog.Id,
+                IsSuccess = true,
+            };
+        }
+    }
+}
