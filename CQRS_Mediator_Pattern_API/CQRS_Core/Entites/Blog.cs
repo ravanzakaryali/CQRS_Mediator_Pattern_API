66a77eb3d02f928e5a8534bd260861d22642693b
revanzli <revanzekeryeli2002@gmail.com>
2022-05-11 14:47:20 +0400

    feat: added blog model

---
+++ CQRS_Mediator_Pattern_API/CQRS_Core/Entites/Blog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CQRS_Core.Entites
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool IsArchive { get; set; }
+        public bool IsDeleted { get; set; }
+        public bool IsModified { get; set; }
+    }
+}
