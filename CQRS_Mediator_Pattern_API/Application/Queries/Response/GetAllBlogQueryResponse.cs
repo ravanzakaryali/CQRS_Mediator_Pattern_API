b6b5a873aed7852e629405601d61f8a080626beb
revanzli <revanzekeryeli2002@gmail.com>
2022-05-11 21:05:43 +0400

    feat: added Queries/Response

---
+++ CQRS_Mediator_Pattern_API/Application/Queries/Response/GetAllBlogQueryResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Application.Queries.Response
+{
+    public class GetAllBlogQueryResponse
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string UserId { get; set; }
+        public bool IsModified { get; set; }
+    }
+}
---
+++ CQRS_Mediator_Pattern_API/Application/Queries/Response/GetBlogQeuryResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Queries.Response
+{
+    public class GetBlogQeuryResponse
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string UserId { get; set; }
+        public bool IsModified { get; set; }
+    }
+}
