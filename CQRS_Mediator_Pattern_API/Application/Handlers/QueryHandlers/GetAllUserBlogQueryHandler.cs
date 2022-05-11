cc9b56b34ff31964b2855dde497fceb119b28223
revanzli <revanzekeryeli2002@gmail.com>
2022-05-11 22:10:32 +0400

    feat: added QueryHandler

---
+++ CQRS_Mediator_Pattern_API/Application/Handlers/QueryHandlers/GetAllUserBlogQueryHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Handlers.QueryHandlers
+{
+    public class GetAllUserBlogQueryHnadler
+    {
+    }
+}
