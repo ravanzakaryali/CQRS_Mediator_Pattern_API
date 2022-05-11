6f3dd8b9730baa3dab16d4be643964bc64fa9b90
revanzli <revanzekeryeli2002@gmail.com>
2022-05-11 17:42:24 +0400

    feat: added DeleteBlogCommand

---
+++ CQRS_Mediator_Pattern_API/Application/Commands/Request/DeleteBlogCommandRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Commands.Request
+{
+    public class DeleteBlogCommandRequest
+    {
+        public int Id { get; set; }
+    }
+}
