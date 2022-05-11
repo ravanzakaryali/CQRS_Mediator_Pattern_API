51ef8049c467d1e636f3853c33948baf86f753e6
revanzli <revanzekeryeli2002@gmail.com>
2022-05-11 14:39:04 +0400

    feat: added identity user

---
+++ CQRS_Mediator_Pattern_API/CQRS_Core/Entites/User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CQRS_Core.Entites
+{
+    public class User : IdentityUser
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime Birthday { get; set; }
+        public DateTime RegisterDate { get; set; }
+    }
+}
