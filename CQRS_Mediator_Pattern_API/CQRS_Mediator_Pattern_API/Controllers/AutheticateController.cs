863687d028c7a6dbd61342a06d69cac87b2f676f
revanzli <revanzekeryeli2002@gmail.com>
2022-05-12 07:21:41 +0400

    feat: create user

--- CQRS_Mediator_Pattern_API/CQRS_Mediator_Pattern_API/Controllers/AutheticateController.cs
+++ CQRS_Mediator_Pattern_API/CQRS_Mediator_Pattern_API/Controllers/AutheticateController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
 
 namespace CQRS_Mediator_Pattern_API.Controllers
 {
@@ -7,5 +11,26 @@ namespace CQRS_Mediator_Pattern_API.Controllers
     [ApiController]
     public class AutheticateController : ControllerBase
     {
+        private readonly UserManager<User> _userManager;
+
+        public AutheticateController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpPost]
+        public async Task<ActionResult> CreateUser()
+        {
+            var user = new User
+            {
+                Address = "Azerbaijan",
+                Email = "r@gmail.com",
+                Firstname = "Revan",
+                Lastname = "Zakaryali",
+                Birthday = new DateTime(2021, 11, 20),
+            };
+            await _userManager.CreateAsync(user);
+            await _userManager.CreateAsync(user, "R123456@");
+            return NoContent();
+        } 
     }
 }
